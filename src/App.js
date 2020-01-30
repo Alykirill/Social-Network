227a7364891202858cd0ace4075636c7ae77c97c
Kirill Aleshkin <kirilleitan@gmail.com>
2020-01-31 01:00:29 +0200

    add to github pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import {HashRouter, Route, withRouter} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginPage from "./components/Login/Login";
+// import LoginPage from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
+
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
+const LoginPage = React.lazy(() => import("./components/Login/Login"));
+
 
 class App extends Component {
     componentDidMount() {
@@ -24,23 +27,23 @@ class App extends Component {
         }
 
         return (
-                    <div className='app-wrapper'>
-                        <HeaderContainer/>
-                        <Navbar/>
-                        <div className='app-wrapper-content'>
-                            <Route path='/dialogs'
-                                   render={() => <DialogsContainer/>}/>
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Suspense fallback={<Preloader/>}>
+                        <Route path='/dialogs'
+                               render={() => <DialogsContainer/>}/>
+                        <Route path='/profile/:userId?'
+                               render={() => <ProfileContainer/>}/>
+                        <Route path='/login'
+                               render={() => <LoginPage/>}/>
+                    </Suspense>
 
-                            <Route path='/profile/:userId?'
-                                   render={() => <ProfileContainer/>}/>
-
-                            <Route path='/users'
-                                   render={() => <UsersContainer/>}/>
-
-                            <Route path='/login'
-                                   render={() => <LoginPage/>}/>
-                        </div>
-                    </div>
+                    <Route path='/users'
+                           render={() => <UsersContainer/>}/>
+                </div>
+            </div>
         )
     }
 }
@@ -49,17 +52,17 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
 
-let AppContainer  = compose(
+let AppContainer = compose(
     withRouter,
     connect(mapStateToProps, {initializeApp}))(App);
 
-const SamuraiJSApp = (props) =>{
+const SamuraiJSApp = (props) => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <AppContainer/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers:     {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "0ea2a23c-fdf2-4f40-9e11-8ca38ed04f75"
     }
 });
 
